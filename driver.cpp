2cecc88d14ce0b1259d49e2af6a4319728006bcd
joor2163 <49216265+joor2163@users.noreply.github.com>
2019-04-15 17:06:49 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -4,24 +4,6 @@
 
 using namespace std;
 
-string LowerCaseName(string name){
-    int letter;
-    char n;
-    string UpperCaseWord;
-    for(int i = 0; i<name.length(); i++){
-        if(name[i] >= 'a' && name[i] <= 'z'){//It checks if the letter at that index is a lowercaseletter
-            n = int(name[i]);
-            letter = n - 32;
-            UpperCaseWord = UpperCaseWord + char(letter);  //This converts the letter into uppercase and add it into a new string.
-        }else if(name[i] != ' ' && name[i] >= 'A' && name[i] <= 'Z'){ //if the letter ia already in a uppercase it already addeds to a string
-            n = int(name[i]);
-            letter = n;
-            UpperCaseWord = UpperCaseWord + char(letter);
-        }
-    }
-    return UpperCaseWord;
-}
-
 int main(){
   cout << "Hi welcome to scrabble helper." << endl;
   bool flag = false;
@@ -31,8 +13,8 @@ int main(){
   while(flag == false){
     cout << "Please enter the 7 letters that you have" << endl;
     cin >> userAnswer;
-    upperCaseTile = LowerCaseName(userAnswer);
-    cout << "You inputed: " << upperCaseTile << endl;
+    //upperCaseTile = LowerCaseName(userAnswer);
+    cout << "You inputed: " << userAnswer << endl;
     cout << "Are this you tiles? Press Y for yes and N for no." << endl;
     char choose;
     cin >> choose;
