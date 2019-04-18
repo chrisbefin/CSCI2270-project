71245ba67d2830f705b79623a77e3ae0fd36aeaa
Andrew McQueary <andrew@mcqueary.org>
2019-04-18 12:41:12 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "wordArray.hpp"
 
 using namespace std;
@@ -23,8 +24,6 @@ number of words by length
 13 letters- 14185
 14 letters- 9312
 15 letters- 5877
-
-
   return 0;
 max score - 47 points
 */
@@ -48,101 +47,190 @@ int calcScore(string word)
         else if(word[i] == 'Q' || word[i] == 'Z')
             sum += 10;
     }
-    return sum; 
+    return sum;
+}
+
+void dispLayMenu(){
+  cout << "======Main Menu======" << endl;
+  cout << "1. Enter new tiles" << endl;
+  cout << "2. Get best word (no multipliers)" << endl;
+  cout << "3. Check if word is valid" << endl;
+  cout << "5. Quit" << endl;
+}
+
+string UpperCaseName(string name){
+    int letter;
+    char n;
+    string UpperCaseWord;
+    for(int i = 0; i<name.length(); i++){
+        if(name[i] >= 'a' && name[i] <= 'z'){
+            n = int(name[i]);
+            letter = n - 32;
+            UpperCaseWord = UpperCaseWord + char(letter);
+        }else if(name[i] != ' ' && name[i] >= 'A' && name[i] <= 'Z'){
+            n = int(name[i]);
+            letter = n;
+            UpperCaseWord = UpperCaseWord + char(letter);
+        }
+    }
+    return UpperCaseWord;
+}
+
+
+int checkString(string word){
+  int counter = 0;
+  for(int i = 0; i < word.length(); i++){
+    if(word[i] >= 'A' && word[i] <= 'Z'){
+      counter++;
+    }
+  }
+  return counter;
 }
 
 int main(){
     ifstream myStream("ScrabbleDict.txt");
-    wordArray words[15];//array of objects
+    vector<wordArray> words;
+    long int sizes[] = {124,1341,5625,12917,22938,34167,41882,42290,36593,28617,20775,14185,9312,5877};
+    //                   2 ,  3 ,  4 ,  5  ,  6  ,  7  ,  8  ,  9  , 10  , 11  , 12  , 13  , 14 , 15
+    for (int i=0;i<14;i++) {
+      wordArray n(sizes[i]*1.3);
+      words.push_back(n);
+    }
     string word;
     int length;
     int score;
     while(getline(myStream, word))
     {
-        length = word.length();
+        length = word.length()-1;
         score = calcScore(word);
+        word = word.substr(0,length);
         switch(length)
         {
-            case 1:
+            case 2:
             words[0].addWord(score, word);
             break;
-            case 2:
+            case 3:
             words[1].addWord(score, word);
             break;
-            case 3:
+            case 4:
             words[2].addWord(score, word);
             break;
-            case 4:
+            case 5:
             words[3].addWord(score, word);
             break;
-            case 5:
+            case 6:
             words[4].addWord(score, word);
             break;
-            case 6:
+            case 7:
             words[5].addWord(score, word);
             break;
-            case 7:
+            case 8:
             words[6].addWord(score, word);
             break;
-            case 8:
+            case 9:
             words[7].addWord(score, word);
             break;
-            case 9:
+            case 10:
             words[8].addWord(score, word);
             break;
-            case 10:
+            case 11:
             words[9].addWord(score, word);
             break;
-            case 11:
+            case 12:
             words[10].addWord(score, word);
             break;
-            case 12:
+            case 13:
             words[11].addWord(score, word);
             break;
-            case 13:
+            case 14:
             words[12].addWord(score, word);
             break;
-            case 14:
-            words[13].addWord(score, word);
-            break;
             case 15:
-            words[14].addWord(score, word);
+            words[13].addWord(score, word);
             break;
         }
     }
-//   cout << "Hi welcome to scrabble helper." << endl;
-//   string userInput;
-//   while(!false){
-//     cout << "Please enter the 7 letters that you have" << endl;
-//     cin >> userInput;
-//     cout << "You inputed: " << userInput << endl;
-//     cout << "Are this you tiles? Press Y for yes and N for no." << endl;
-//     char choose;
-//     cin >> choose;
-//     while(!false){
-//       if(choose == 'y' || choose == 'Y'){
-//         break;
-//       }else if(choose == 'n' || choose == 'N'){
-//         break;
-//       }else{
-//         cout << "That is not a valid input. Please enter a valid input." << endl;
-//         cin >> choose;
-//       }
-//     }
-//     break;
-//   }
-  string tiles = "ZZZ";
-  int resultScore = 30;
-  node* temp;
-  temp = words[2].searchTiles(resultScore, tiles);
-  if(temp != nullptr)
-  {
-      cout << temp -> word << endl;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+  string userChoose;
+  string userTile;
+  string read;
+  string newUserTile;
+  cout << "Hi welcome to Scrabble Helper" << endl;
+  cout << "Please enter the 7 letters" << endl;
+  getline(cin, userTile);
+  newUserTile = UpperCaseName(userTile);
+  if(checkString(newUserTile) != 7){
+    while(!false){
+      cout << "This input is not valid. Please enter the tiles correctly" << endl;
+      getline(cin, userTile);
+      newUserTile = UpperCaseName(userTile);
+      if(checkString(newUserTile) == 7){
+        break;
+      }
+    }
   }
-  else {
-      cout << "fail"<< endl;
-  }
-  //words[2].print();
+  score = calcScore(newUserTile);
+  cout << "You have entered this 7 letters: " << newUserTile << endl << endl;
+  while(userChoose != "5"){
+    dispLayMenu();
+    getline(cin, userChoose);
+    cout << endl;
+    if(userChoose >= "1" && userChoose <= "9"){
+      switch(stoi(userChoose)){
+        case 1: {
+          cout << "Please enter the new letters" << endl;
+          getline(cin, userTile);
+          newUserTile = UpperCaseName(userTile);
+          if(checkString(newUserTile) != 7){
+            while(!false){
+              cout << "This input is not valid. Please enter the tiles correctly" << endl;
+              getline(cin, userTile);
+              newUserTile = UpperCaseName(userTile);
+              if(checkString(newUserTile) == 7){
+                break;
+              }
+            }
+          }
+        }
+          cout << "You have entered these 7 letters: " << newUserTile << endl << endl;
+          score = calcScore(newUserTile);
+          break;
+        case 2: {
+          node *pres = words[5].searchTiles(score,newUserTile);
+          if (pres == NULL) {
+            cout << "Fail" << endl << endl;
+          }
+          else {
+            cout<< pres->word << " is the best word you can play" << endl << endl;
+          }
+          break;
+        }
+        case 3: {
+          string input;
+          getline(cin, input);
+          input = UpperCaseName(input);
+          cout << input;
+          if (words[input.length()-2].searchWord(calcScore(input),input)) {
+            cout <<" is valid" << endl;
+          }
+          else {
+            cout << " is not valid" << endl;
+          }
+          cout << endl;
+          break;
+        }
+        case 5:
+          cout << "Goodbye! " << endl << endl;
+          return 0;
+          break;
+        default:
+          cout << "That is not a valid input. Please enter a valid input." << endl;
+          break;
+        }
+      }else{
+        cout << "That is not a valid input. Please enter a valid input." << endl;
+      }
+    }
   return 0;
-  
+
 }
